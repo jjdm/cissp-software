7a8be885f516591e0cc31226563d34d516b5c1a8
Josh Martin <josh.martin@gmail.com>
2017-11-26 13:02:42 -0500

    Cleaning up JSON

--- docs/examples/web/04_bootstrap/app.js
+++ docs/examples/web/04_bootstrap/app.js
@@ -7,17 +7,17 @@ app.controller('ListController', function ($scope, $log) {
 	$scope.description = null;
 
 	$scope.items = [
-		{"id": 1, "name": "Network Event", "description": "Bandwidth anomoly detected.  Potential DDOS."},
-		{"id": 2, "name": "Sensor Alert", "description": "SNORT sensor flagged known IP."}
+		{id: 1, name: "Network Event", description: "Bandwidth anomoly detected.  Potential DDOS."},
+		{id: 2, name: "Sensor Alert", description: "SNORT sensor flagged known IP."}
 	]
 
 	$scope.addItem = function () {
 		$scope.maxId++;
 		$log.debug("Adding item " + $scope.maxId);
 		$scope.items.push({
-			"id": $scope.maxId,
-			"name": $scope.name,
-			"description": $scope.description
+			id: $scope.maxId,
+			name: $scope.name,
+			description: $scope.description
 		});
 		$scope.name = null;
 		$scope.description = null;
